80a55e7846fd75439459be6dfcd3a5308d43c384
camilanb <66702531+camilanb@users.noreply.github.com>
2021-05-10 16:51:57 -0300

    new

--- js/script.js
+++ js/script.js
@@ -1,10 +1,4 @@
-if(document.getElementById("nome-usuaria").value != "" && document.getElementById("email-usuaria").value != ""){
-alert("Seus dados foram recebidos com sucesso!")
-}else{
-alert("Por favor, preencha os dados para serem enviados")
-}
 
-<script>
                 function funcao1(){
                 if(document.getElementById("nome-usuaria").value != "" && document.getElementById("email-usuaria").value != "") {
                 alert("Seus dados foram recebidos com sucesso!")
@@ -12,4 +6,3 @@ alert("Por favor, preencha os dados para serem enviados")
                 alert("Por favor, preencha os dados para serem enviados")
                 }
                 }
-                </script>
